d73186067acb4d28fedfc5d0278d457e5c20fe90
John Papa <john@johnpapa.net>
2017-02-22 22:23:19 -0500

    init

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AngularAspNetCore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("~/Views/Index.cshtml");
+        }
+    }
+}
